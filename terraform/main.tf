d9a0c45002eccfb2b7f168238519da63acade8d8
krishnath.poologanathan <pkrishnath@gmail.com>
2025-09-18 14:01:44 +0100

    fixed IP address issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,8 @@ resource "azurerm_public_ip" "ten_validatorApp_terraform_public_ip" {
   name                = "ten_validatorPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
   domain_name_label   = "tenvalidatorvm"
 }
 
